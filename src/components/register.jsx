8bf549885d6716012dc8a9a89935ddc2d648d978
juandejunin <juandejunin75@gmail.com>
2025-02-06 07:25:36 +0100

    feat: add detall funnel component

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -4,13 +4,13 @@ function Formulario() {
   const [nombre, setNombre] = useState('');
   const [email, setEmail] = useState('');
   const [mensaje, setMensaje] = useState('');
-  const [error, setError] = useState(''); 
+  const [error, setError] = useState('');
   const [enviado, setEnviado] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setError('');  
-    setMensaje(''); 
+    setError('');
+    setMensaje('');
 
     try {
       const response = await fetch('http://localhost:3000/api/users/register', {
@@ -28,7 +28,7 @@ function Formulario() {
         setError(data.error || 'Ocurrió un error al enviar el formulario.');
       }
 
-      setEnviado(true); 
+      setEnviado(true);
     } catch (error) {
       console.error('Error:', error);
 
@@ -36,45 +36,49 @@ function Formulario() {
   };
 
   return (
-     <div className="max-w-lg mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
-      {enviado ? (
-        <div className="text-green-500 font-bold">{mensaje}</div>
-      ) : (
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <h1 className="text-2xl font-semibold text-center">Registro</h1>
-          <div>
-            <label htmlFor="nombre" className="block text-lg">Nombre:</label>
-            <input
-              type="text"
-              id="nombre"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-              required
-              className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="block text-lg">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full py-2 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            Enviar
-          </button>
-          {error && error !== '' && <div className="text-red-500 mt-2">{error}</div>}
+    <section  id="formulario" className="min-h-screen w-full flex items-center justify-center p-8 bg-primary">
+      <div  className=" w-full max-w-md mx-auto mt-10 p-8 bg-secondary shadow-lg rounded-lg">
 
-        </form>
-      )}
-    </div>
+        {enviado ? (
+          <div className="text-green-500 font-bold">{mensaje}</div>
+        ) : (
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <h1 className="text-2xl text-gray-400 font-semibold text-center">Solicita tu guia</h1>
+            <div>
+              <label htmlFor="nombre" className="block text-lg text-gray-400">Nombre:</label>
+              <input
+                type="text"
+                id="nombre"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                required
+                className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-lg text-gray-400">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full p-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 mt-4 bg-light rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              Enviar
+            </button>
+            {error && error !== '' && <div className="text-red-500 mt-2">{error}</div>}
+
+          </form>
+        )}
+      </div>
+    </section>
+
   );
 }
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#080436', // Nombre personalizado para tu color
+				secondary: '#1E1A55',
+				accent: '#FFD700',
+				light: '#FEB95F',
+				background: '#0A092D'
+
+
+
+
+			},
+		},
 	},
 	plugins: [],
 }
